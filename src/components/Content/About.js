4f8daa9421ec32560541dc674aa9f82fd4c41a12
Fazer <fazzer.crypto@hotmail.com>
2021-10-18 23:09:24 +0200

    test of typewriter

--- src/components/Content/About.js
+++ src/components/Content/About.js
@@ -2,41 +2,59 @@ import { React, useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import ImageSlider from "./ImageSlider";
-import Typewriter from 'typewriter-effect/dist/core';
+import TypeWriterEffect from 'react-typewriter-effect';
 
 const About = () => {
     useEffect(() => {
         AOS.init();
         AOS.refresh();
-      });
-
-      var app = document.getElementById('app');
-
-    var typewriter = new Typewriter(app, {
-     loop: true,
-     delay: 75,
     });
-
-typewriter
-  .pauseFor(2500)
-  .typeString('A simple yet powerful native javascript')
-  .pauseFor(300)
-  .deleteChars(10)
-  .typeString('<strong>JS</strong> plugin for a cool typewriter effect and ')
-  .typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
-  .pauseFor(1000)
-  .start();
 
     return (
         <div className="container">
-            
+
             <div data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine" className="about-info" id="about">
                 <h1>About</h1>
-                <p>We write the year 2221. Moon Enceladus - Saturn.</p>
-                <p>Resources are scarce, and the greatest source of energy lies in the power of Gemesis Stones.</p>
-
+                <TypeWriterEffect
+                     textStyle={{
+                        fontFamily: 'Red Hat Display',
+                        color: '#3F3D56',
+                        fontWeight: 500,
+                        fontSize: '1.5em',
+                    }}
+                    startDelay={100}
+                    cursorColor="#3F3D56"
+                    text="We write the year 2221. Moon Enceladus - Saturn."
+                    typeSpeed={30}
+                    hideCursorAfterText="true"
+                />
+                <TypeWriterEffect
+                     textStyle={{
+                        fontFamily: 'Red Hat Display',
+                        color: '#3F3D56',
+                        fontWeight: 500,
+                        fontSize: '1.5em',
+                    }}
+                    startDelay={3000}
+                    cursorColor="#3F3D56"
+                    text="Resources are scarce, and the greatest source of energy lies in the power of the Gemesis Stones."
+                    typeSpeed={30}
+                    hideCursorAfterText="true"
+                />
+                <TypeWriterEffect
+                     textStyle={{
+                        fontFamily: 'Red Hat Display',
+                        color: '#3F3D56',
+                        fontWeight: 500,
+                        fontSize: '1.5em',
+                    }}
+                    startDelay={9000}
+                    cursorColor="#3F3D56"
+                    text="To be continued..."
+                    typeSpeed={30}
+                    hideCursorAfterText="true"
+                />
             </div>
-
             <ImageSlider />
         </div>
 
