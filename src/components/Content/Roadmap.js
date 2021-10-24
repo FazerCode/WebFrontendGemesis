018c18d815cbc6aab7839f9da0f4a9708358ce78
Fazer <fazzer.crypto@hotmail.com>
2021-10-24 11:16:19 +0200

    removed some unofficial content

--- src/components/Content/Roadmap.js
+++ src/components/Content/Roadmap.js
@@ -8,20 +8,11 @@ const Roadmap = () => {
         AOS.refresh();
       });
 
-      // TODO https://stackoverflow.com/questions/52129967/react-to-mouse-click-event-on-image-at-certain-coordinates/52130230
-      // get position of img and display popover of position
-
     return (
         <div className="roadmap background2" >
             <div className="roadmap-info container" id="roadmap" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
-                <h1>Treasure Map</h1>
-                <h1>Treasure Map</h1>
-                <h1>Treasure Map</h1>
-                <h1>Treasure Map</h1>
-                <h1>Treasure Map</h1>
-                <h1>Treasure Map</h1>
-                <h1>Treasure Map</h1>           
-
+                <h1>Roadmap</h1>
+                <h1>To be announced...</h1>
             </div>
         </div>
     )
